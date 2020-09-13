b268f6e38b620eb7bb4e4a14414e35c6d3dd4a92
unclepeeh <52495702+unclepeeh@users.noreply.github.com>
2020-09-13 16:43:38 +0100

    Add files via upload

---
+++ cfg.php
@@ -0,0 +1,14 @@
+<?php
+// -*- Copyleft @ °✍⎯⚚͜͡SʰⁱNᵒᵇⁱ - 2020 -*-
+// -*- Config GlobalRtx -*-
+// -*- If you want to donate, at DOGE Wallet:
+// -*- DDsu3tCfqXbhoj1j84Ydq3qkBYNZZESC3Q
+//________________________________________________
+
+// Register at https://bit.ly/31L6Z5M
+// Input your login data in here || Tulis data login kamu di bawah
+
+$mail = "itiranking@gmail.com"; 
+$passwd = "pploveosa1"; 
+
+?>
